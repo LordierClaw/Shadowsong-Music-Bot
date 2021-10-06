d6614c6b7df5892c715c14e0a454f7f5586f8188
LordierClaw <lordierclaw@gmail.com>
2021-10-06 18:46:42 +0700

    Add join and leave command

--- Shadowsong/cogs/music.py
+++ Shadowsong/cogs/music.py
@@ -5,5 +5,28 @@ class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name="connect", aliases=["join"])
+    async def join(self, ctx):
+        if ctx.author.voice:
+            if ctx.voice_client is None:
+                await ctx.author.voice.channel.connect()
+                await ctx.send("Connected!")
+            else:
+                await ctx.voice_client.move_to(ctx.author.voice.channel)
+                await ctx.send("I'm already in voice channel")
+        else:
+            await ctx.send("You haven't connected to a voice channel.")
+
+    @commands.command(name="disconnect", aliases=["leave"])
+    async def leave(self, ctx):
+        server_id = ctx.message.guild.id
+        try:
+            if ctx.voice_client.is_playing():
+                ctx.voice_client.stop()
+            await ctx.voice_client.disconnect()
+            await ctx.send("Disconnected!")
+        except AttributeError:
+            await ctx.send("I'm not in any voice channel")
+
 def setup(bot):
     bot.add_cog(Music(bot))
